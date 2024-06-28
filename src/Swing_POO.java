0cffb48ac176873d8d0105cbef2a222e044fc099
RichardPadilla7 <richardpadilla3rod@gmail.com>
2024-06-27 19:38:21 -0500

    Base de la calculadora

--- src/Swing_POO.java
+++ src/Swing_POO.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Swing_POO {
+import javax.swing.*;
+
+public class Swing_POO {
+    public JTextArea Titulo;
+    public JButton Suma;
+    public JButton Resta;
+    public JButton Multi;
+    public JButton Division;
+    public JButton Raiz;
+    public JButton Potencia;
+    public JButton Seno;
+    public JButton Coseno;
+    public JButton Tangente;
+    public JButton calcularButton;
+    public JTextField Numero1;
+    public JTextField Numero2;
+    public JTextPane Respuesta;
+    public JPanel Calculadora;
+
+
+
+
+
+
+
+
+
 }
+
