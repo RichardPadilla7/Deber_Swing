9fe713c96bbd5794f98843aaebe7b170c6c1fd30
Kevin <kevin.mendoza01@epn.edu.ec>
2024-06-28 14:54:23 -0500

    K3

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Calculadora");
+        System.out.println("Primer intento");
+        JFrame frame = new JFrame("Esta es nuestra Calculadora");
         frame.setContentPane(new Swing_POO().Calculadora);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700, 700);
         frame.pack();
         frame.setVisible(true);
 // WALTER COBACANGO, RICHARD PADILLA, KEVIN MENDOZA...
+
     }
 }
